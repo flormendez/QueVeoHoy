2b746f4c0c22ae709bfda961d91bd3c25e138c1c
Florencia <florenciamendez93@gmail.com>
2019-02-12 08:53:40 -0300

    Filtros completos

--- servidor/controladores/controller.js
+++ servidor/controladores/controller.js
@@ -1,67 +1,64 @@
 var con = require("../lib/conexionbd.js");
 
-
-
-function armarConsulta(req){
+function armarConsulta(req) {
   var sql = " from pelicula";
   var hayFiltro = false;
 
-  if(req.query.titulo || req.query.anio || req.query.genero){
-    sql = sql.concat(" where ")
+  if (req.query.titulo || req.query.anio || req.query.genero) {
+    sql = sql.concat(" where ");
   }
 
-  if(req.query.titulo){
-    if(hayFiltro){
-      sql = sql.concat(" and ")
+  if (req.query.titulo) {
+    sql = sql.concat(" titulo like % " + req.query.titulo + "%");
+    var hayFiltro = true;
+  }
+  if (req.query.genero) {
+    if (hayFiltro) {
+      sql = sql.concat(" and ");
     }
-    sql=sql.concat(" titulo = " + req.query.titulo)
+    sql = sql.concat(" genero_id = " + req.query.genero);
+    var hayFiltro = true;
   }
-   if(req.query.genero){
-    if(hayFiltro){
-      sql = sql.concat(" and ")
+  if (req.query.aio) {
+    if (hayFiltro) {
+      sql = sql.concat(" and ");
     }
-    sql=sql.concat(" genero = " + req.query.genero)
+    sql = sql.concat(" anio = " + req.query.anio);
+    var hayFiltro = true;
   }
-  if(req.query.aio){
-    if(hayFiltro){
-      sql = sql.concat(" and ")
-    }
-    sql=sql.concat(" anio = " + req.query.anio)
+  if (req.query.tipo_orden) {
+    sql = sql.concat(
+      " order by " + req.query.columna_orden + " " + req.query.tipo_orden
+    );
+    var hayFiltro = true;
   }
-  if(req.query.tipo_orden){
-    sql = sql.concat(" order by " + req.query.columna_orden + " " + req.query.tipo_orden)
+  if (req.query.cantidad) {
+    sql = sql.concat(" limit 0," + req.query.cantidad);
+    var hayFiltro = true;
   }
-  if(req.query.cantidad){
-    sql=sql.concat(" limit 0," + req.query.cantidad)
-  }
-  sql= sql.concat("; ")
-  return sql; 
-
+  sql = sql.concat(";");
+  return sql;
 }
 
-
-
-
-
-
 function devolverTodas(req, res) {
-  var sql = "select * from pelicula";
-  con.query(sql, function(error, resultado, fields) {
-    if (error) {
-      console.log("Hubo un error en la consulta", error.message);
-      return res.status(404).send("Hubo un error en la consulta");
-    }
-    var response = {
-      peliculas: resultado
-    };
+  var query = armarConsulta(req);
+  var sql = "select *".concat(query);
+  var count = "select count(id) as total".concat(query);
+  con.query(sql, function(error, resultado) {
+    con.query(count, function(errorCount, resultadoCount) {
+      var response = {
+        peliculas: resultado,
+        total: resultadoCount[0].total
+      };
 
-    res.send(JSON.stringify(response));
+      res.send(JSON.stringify(response));
+    });
   });
 }
 
 function devolverGeneros(req, res) {
   var sql = "select * from genero";
-  con.query(sql, function(error, resultado, fields) {
+  con.query(sql, function(error, resultado) {
     if (error) {
       console.log("Hubo un error en la consulta", error.message);
       return res.status(404).send("Hubo un error en la consulta");
