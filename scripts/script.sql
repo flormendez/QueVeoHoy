081d211087a6323d041c72ebfe057c85a9342958
Florencia <florenciamendez93@gmail.com>
2019-03-22 08:32:56 -0300

    modificio script

--- scripts/script.sql
+++ scripts/script.sql
@@ -1,16 +1,38 @@
-CREATE DATABASE peliculas;
+CREATE DATABASE IF NOT EXISTS Peliculas;
 
-USE peliculas;
+USE Peliculas;
 
-CREATE TABLE `pelicula` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `titulo` varchar(100) DEFAULT NULL,
-  `duracion` INT DEFAULT NULL,
-  `director` varchar(400) DEFAULT NULL,
-  `anio` INT DEFAULT NULL,
-  `fecha_lanzamiento` DATE DEFAULT NULL,
-  `puntuacion` INT DEFAULT NULL,
-   `poster` varchar(300) DEFAULT NULL,
-   `trama` varchar(700) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS pelicula(
+    id INT NOT NULL AUTO_INCREMENT,
+    titulo VARCHAR(100) NOT NULL,
+    duracion SMALLINT NOT NULL,
+    director VARCHAR(400) NOT NULL,
+    anio  SMALLINT NOT NULL,
+    fecha_lanzamiento DATE NOT NULL,
+    puntuacion SMALLINT NOT NULL,
+    poster VARCHAR(300) NOT NULL,
+    trama VARCHAR(700) NOT NULL,
+   PRIMARY KEY (id)
+);
+
+ALTER TABLE pelicula
+ADD COLUMN genero_id INT;
+
+CREATE TABLE IF NOT EXISTS genero(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS actor(
+    id INT NOT NULL AUTO_INCREMENT,
+    nombre VARCHAR(70) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS actor_pelicula(
+    id INT NOT NULL AUTO_INCREMENT
+    actor_id INT NOT NULL,
+    pelicula_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
